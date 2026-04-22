2e1b794a7e6cef8c6d93e5b67eb1d25ea63bfe3c
Ivan Knezevic <knezivan.au@gmail.com>
2026-04-22 22:02:05 +1000

    Wrap LoginPage in QLayout (QPage needs a layout ancestor)

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,45 +1,49 @@
 <template>
-<q-page padding class="row justify-center items-start">
-    <q-card style="max-width: 420px; width: 100%;" class="q-mt-xl">
-        <q-card-section>
-            <div class="text-h5">Log in</div>
-            <div class="text-caption text-grey-7">CrateMate</div>
-        </q-card-section>
+<q-layout>
+    <q-page-container>
+        <q-page padding class="row justify-center items-start">
+            <q-card style="max-width: 420px; width: 100%;" class="q-mt-xl">
+                <q-card-section>
+                    <div class="text-h5">Log in</div>
+                    <div class="text-caption text-grey-7">CrateMate</div>
+                </q-card-section>
 
-        <q-form @submit.prevent="onSubmit">
-            <q-card-section class="q-gutter-md">
-                <q-input
-                    v-model="form.email"
-                    label="Email"
-                    outlined
-                    type="email"
-                    autocomplete="email"
-                    :rules="[ ( val ) => !!val || 'Email is required' ]"
-                />
-                <q-input
-                    v-model="form.password"
-                    label="Password"
-                    outlined
-                    type="password"
-                    autocomplete="current-password"
-                    :rules="[ ( val ) => !!val || 'Password is required' ]"
-                />
-                <q-banner v-if="error" rounded class="bg-negative text-white">
-                    {{ error }}
-                </q-banner>
-            </q-card-section>
+                <q-form @submit.prevent="onSubmit">
+                    <q-card-section class="q-gutter-md">
+                        <q-input
+                            v-model="form.email"
+                            label="Email"
+                            outlined
+                            type="email"
+                            autocomplete="email"
+                            :rules="[ ( val ) => !!val || 'Email is required' ]"
+                        />
+                        <q-input
+                            v-model="form.password"
+                            label="Password"
+                            outlined
+                            type="password"
+                            autocomplete="current-password"
+                            :rules="[ ( val ) => !!val || 'Password is required' ]"
+                        />
+                        <q-banner v-if="error" rounded class="bg-negative text-white">
+                            {{ error }}
+                        </q-banner>
+                    </q-card-section>
 
-            <q-card-actions align="right" class="q-pa-md">
-                <q-btn
-                    color="primary"
-                    label="Log in"
-                    type="submit"
-                    :loading="loading"
-                />
-            </q-card-actions>
-        </q-form>
-    </q-card>
-</q-page>
+                    <q-card-actions align="right" class="q-pa-md">
+                        <q-btn
+                            color="primary"
+                            label="Log in"
+                            type="submit"
+                            :loading="loading"
+                        />
+                    </q-card-actions>
+                </q-form>
+            </q-card>
+        </q-page>
+    </q-page-container>
+</q-layout>
 </template>
 
 <script setup>
